d5824d627e993d81dea1e510b8d421e82d75a9a8
MohamedHerz <mohamed.herz96@gmail.com>
2025-12-21 20:38:03 +0300

    ticket component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,38 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react"
+import axios from "axios"
+import "./App.css"
 
+import Ticket from "./components/Ticket"
 
+const App = () => {
+  const [tickets, setTicket] = useState([])
 
+  useEffect(() => {
+    const getTicket = async () => {
+      try {
+        let response = await axios.get("http://localhost:3001/issues")
+        setTicket(response.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    getTicket()
+  }, [])
+
+  return (
+    <>
+      <Form Ticket={tickets} setTicket={setTicket} />
+      <h1>Tickets:</h1>
+      {tickets?.map((ticket) => (
+        <div key={ticket._id}>
+          <h3>Type: {ticket.ticketType}</h3>
+          <p>Subject: {ticket.subject}</p>
+          <p>Message: {ticket.message}</p>
+        </div>
+      ))}
+    </>
+  )
+}
 
 export default App
---
+++ client/src/components/Ticket.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react"
+
+const Form = () => {
+  const initialState = {
+    ticketType: "",
+    subject: "",
+    message: "",
+  }
+
+  const [formState, setFormState] = useState(initialState)
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const response = await axios.post(
+      "http://localhost:3000/tickets",
+      formState
+    )
+    let ticketList = [...tickets]
+    ticketList.push(response.data)
+    setFormState(initialState)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="ticketType">Type of Issue:</label>
+      <select
+        name="ticketType"
+        onChange={handleChange}
+        value={formState.ticketType}
+      >
+        <option value="" disabled defaultValue>
+          Select Ticket Type
+        </option>
+        <option value="sign-in ">Sign-in Issues</option>
+        <option value="bug">Bugs in website</option>
+        <option value="Suggestion">Suggestion</option>
+      </select>
+      <label htmlFor="subject">Subject:</label>
+      <input
+        type="text"
+        name="subject"
+        onChange={handleChange}
+        value={formState.subject}
+        autocomplete="off"
+      />
+      <label htmlFor="message">Message</label>
+      <textarea
+        name="message"
+        cols="30"
+        rows="10"
+        onChange={handleChange}
+        value={formState.message}
+        autocomplete="off"
+      ></textarea>
+      <button type="submit">Send</button>
+    </form>
+  )
+}
+
+export default Form
